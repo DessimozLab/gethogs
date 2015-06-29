73d4140989bc5c12ee82a07e0e4d85755b42df07
traincm <clement.train@gmail.com>
2015-06-29 16:23:04 +0200

    fixed FA cmd ?
    Former-commit-id: 16868585da36ed2486264bb2fa824d9f2bd21f0a
    Former-commit-id: e5affc935346e373fda90c011b5b01ab3527391b [formerly 31b41ea039bb9ef708ff1eec515d325754cf83b6] [formerly 1da48e4064aa35209b39e3ddcaae2013e43854e1 [formerly 78119a3282c7c894bc6cbdc9c4a334ab03d7d946]]
    Former-commit-id: 6bae0cdbd6d1e961a898cd906196adeea43e73d6 [formerly 6ecc04bde4d0ca9a8d0bf2461ed476734aae3ead]
    Former-commit-id: 391defac22a2f59ed1ee2b021a55ec6cb4d92786

--- HOG_algo/pipeline_FA_OMA_bottom.py
+++ HOG_algo/pipeline_FA_OMA_bottom.py
@@ -8,23 +8,13 @@ taxons=["Homininae","Murinae"]
 
 
 
-def write_output_FA_into_file(fn, taxon_arg, taxon):
-    cmd = ["../family-analyser/bin/familyanalyzer", "--xreftag", "omaId", "../Result/"+classes.Settings.dir_name_param+ classes.Settings.xml_name_param, taxon]
-    for arg in taxon_arg:
-        cmd.append(arg)
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
-    out, err = p.communicate()
-    with open("../Result/"+ classes.Settings.dir_name_param+"/FA/"+ fn,"w+") as f:
-        for line in out:
-            f.write(str(line))
-        f.close()
 
 def main_pipeline_FA():
     for taxon in taxons:
         if taxon =='Homininae':
-            os.system("cd ../family-analyzer; bin/familyanalyzer --xreftag protId "+ "../Result/"+classes.Settings.dir_name_param+"/"+ classes.Settings.xml_name_param +" "+ "HUMANPANTR" +" HUMAN PANTR  >"  +"../Result/"+ classes.Settings.dir_name_param+"/FA/"+ "Homininae.txt" )
+            os.system("cd ../family-analyzer; ./bin/familyanalyzer --xreftag protId "+ "../Result/"+classes.Settings.dir_name_param+"/"+ classes.Settings.xml_name_param +" "+ "HUMANPANTR" +" HUMAN PANTR  >"  +"../Result/"+ classes.Settings.dir_name_param+"/FA/"+ "Homininae.txt" )
         elif taxon == "Murinae":
-            os.system("cd ../family-analyzer; bin/familyanalyzer --xreftag protId "+ "../Result/"+classes.Settings.dir_name_param+"/"+ classes.Settings.xml_name_param +" "+ "MOUSERATNO" +" MOUSE RATNO >"  +"../Result/"+ classes.Settings.dir_name_param+"/FA/"+ "Murinae.txt" )
+            os.system("cd ../family-analyzer; ./bin/familyanalyzer --xreftag protId "+ "../Result/"+classes.Settings.dir_name_param+"/"+ classes.Settings.xml_name_param +" "+ "MOUSERATNO" +" MOUSE RATNO >"  +"../Result/"+ classes.Settings.dir_name_param+"/FA/"+ "Murinae.txt" )
 
 '''
 '''
